f56a1fad5f3359cdd36b1b81759addb235f0a219
csauad-hub <csauad@gmail.com>
2026-03-24 18:05:17 -0300

    add lib/supabase/server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,6 +3,6 @@ import { cookies } from 'next/headers'
 export function createClient() {
   const cookieStore = cookies()
   return createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, {
-    cookies: { getAll() { return cookieStore.getAll() }, setAll(c) { try { c.forEach(({name,value,options}) => cookieStore.set(name,value,options)) } catch {} } }
+    cookies: { getAll() { return cookieStore.getAll() }, setAll(cookiesToSet) { try { cookiesToSet.forEach(({ name, value, options }) => cookieStore.set(name, value, options)) } catch {} } }
   })
 }
